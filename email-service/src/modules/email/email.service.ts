0b4398cae1a41012961bf1a3ca1493b3fc64afd4
Muhammad Hamza <hamza2797@hotmail.com>
2023-07-19 20:25:34 +0500

    removed unused dto from email service

--- email-service/src/modules/email/email.service.ts
+++ email-service/src/modules/email/email.service.ts
@@ -1,8 +1,5 @@
-import { Inject, Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Order } from 'src/core/schemas/order.schema';
-import { ClientProxy } from '@nestjs/microservices';
+import { Injectable } from '@nestjs/common';
+
 @Injectable()
 export class EmailService {
   constructor() {}
